d8b7941fcee2647f1328368798e29beaf78d21f3
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-08-16 21:04:27 +0300

    completed the projects page

--- projects.php
+++ projects.php
@@ -17,7 +17,142 @@
 
     <main>
         <!--Page Content-->
+        <!--Square Pattern Start-->
+        <div class="pattern-square"></div>
+        <!--Square Pattern End-->
 
+        <!--Pageheader start-->
+        <section class="py-5 py-lg-8">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-12">
+                        <div class="text-center">
+                            <h1 class="mb-3">Our Projects</h1>
+                            <p class="mb-0 text-center" style="text-align: justify; text-align-last: center;">
+                                Discover how we transform spaces and bring visions to life with precision, innovation, and unmatched quality.
+                            </p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--Pageheader end-->
+
+        <!--career study start-->
+        <section class="mb-xl-9 my-4">
+            <div class="container">
+                <div class="row g-5">
+                    <div class="col-lg-4 col-md-6 col-12">
+                        <div class="card border-0 h-100 card-lift">
+                            <a href="#!"><img src="assets/images/landings/it-company/it-company-case-study-1.jpg" alt="Office Workspace, Workable LLP - Sanlam Towers, 6th Floor (2023)" class="img-fluid rounded-top-3" /></a>
+                            <div class="card-body">
+                                <div class="d-flex flex-column gap-2">
+                                    <div>
+                                        <h4><a href="#!" class="text-reset">Office Workspace</a></h4>
+                                        <p class="mb-1">Workable LLP - Sanlam Towers, 6th Floor</p>
+                                        <p class="mb-3">2023</p>
+                                        <a href="#!" class="btn btn-primary">View Project</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 col-12">
+                        <div class="card border-0 h-100 card-lift">
+                            <a href="#!"><img src="assets/images/landings/it-company/it-company-case-study-2.jpg" alt="Office Fitout in Lavington (2020)" class="img-fluid rounded-top-3" /></a>
+                            <div class="card-body">
+                                <div class="d-flex flex-column gap-2">
+                                    <div>
+                                        <h4><a href="#!" class="text-reset">Office Fitout</a></h4>
+                                        <p class="mb-1">Lavington</p>
+                                        <p class="mb-3">2020</p>
+                                        <a href="#!" class="btn btn-primary">View Project</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 col-12">
+                        <div class="card border-0 h-100 card-lift">
+                            <a href="#!"><img src="assets/images/landings/it-company/it-company-case-study-3.jpg" alt="Aluminium & Glazing Works, The Address, Waiyaki Way (2017)" class="img-fluid rounded-top-3" /></a>
+                            <div class="card-body">
+                                <div class="d-flex flex-column gap-2">
+                                    <div>
+                                        <h4><a href="#!" class="text-reset">Aluminium & Glazing Works</a></h4>
+                                        <p class="mb-1">The Address, Waiyaki Way</p>
+                                        <p class="mb-3">2017</p>
+                                        <a href="#!" class="btn btn-primary">View Project</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 col-12">
+                        <div class="card border-0 h-100 card-lift">
+                            <a href="#!"><img src="assets/images/landings/it-company/it-company-case-study-4.jpg" alt="AKUHN ETB 4th Floor - Parklands (2023)" class="img-fluid rounded-top-3" /></a>
+                            <div class="card-body">
+                                <div class="d-flex flex-column gap-2">
+                                    <div>
+                                        <h4><a href="#!" class="text-reset">Office Fitout</a></h4>
+                                        <p class="mb-1">AKUHN ETB 4th Floor - Parklands</p>
+                                        <p class="mb-3">2023</p>
+                                        <a href="#!" class="btn btn-primary">View Project</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 col-12">
+                        <div class="card border-0 h-100 card-lift">
+                            <a href="#!"><img src="assets/images/landings/it-company/it-company-case-study-5.jpg" alt="Mahogany Hardwood CNC Designed Queen Bed (Custom Project)" class="img-fluid rounded-top-3" /></a>
+                            <div class="card-body">
+                                <div class="d-flex flex-column gap-2">
+                                    <div>
+                                        <h4><a href="#!" class="text-reset">Custom Project</a></h4>
+                                        <p class="mb-1">Mahogany Hardwood CNC Designed Queen Bed</p>
+                                        <p class="mb-3">2023</p>
+                                        <a href="#!" class="btn btn-primary">View Project</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 col-12">
+                        <div class="card border-0 h-100 card-lift">
+                            <a href="#!"><img src="assets/images/landings/it-company/it-company-case-study-6.jpg" alt="Sponsorship of Sikh Golfing Society (SGS) Tournament at Vetlab Sports Club (2023)" class="img-fluid rounded-top-3" /></a>
+                            <div class="card-body">
+                                <div class="d-flex flex-column gap-2">
+                                    <div>
+                                        <h4><a href="#!" class="text-reset">Event Sponsorship</a></h4>
+                                        <p class="mb-1">Sikh Golfing Society (SGS) Tournament</p>
+                                        <p class="mb-3">2023</p>
+                                        <a href="#!" class="btn btn-primary">View Project</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-12 text-center">
+                        <div class="mt-xl-7 mt-3">
+                            <a class="btn btn-outline-primary" href="#!" id="loadMoreButton">
+                                <span class="spinner-border spinner-border-sm d-none" role="status" aria-hidden="true" id="spinner"></span>
+                                <span class="ms-2">Load More</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </section>
+        <!--career study end-->
     </main>
 
     <!-- Footer -->
@@ -26,6 +161,15 @@
 
     <!-- Scripts -->
     <?php include 'includes/scripts.php'; ?>
+
+    <!-- Load More Spinner -->
+    <script>
+        document.getElementById('loadMoreButton').addEventListener('click', function() {
+            const spinner = document.getElementById('spinner');
+            spinner.classList.remove('d-none');
+            // Load more functionality
+        });
+    </script>
 
 </body>
 
