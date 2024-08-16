f046c0749410c2222e716dbd432484292f926af9
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-08-16 14:39:30 +0300

    added the global components and starter page

---
+++ includes/scripts.php
@@ -0,0 +1,11 @@
+    <!-- Libs JS -->
+    <script src="assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/libs/simplebar/dist/simplebar.min.js"></script>
+    <script src="assets/libs/headhesive/dist/headhesive.min.js"></script>
+
+    <!-- Theme JS -->
+    <script src="assets/js/theme.min.js"></script>
+
+    <script src="assets/js/vendors/tab-to-dropdown.js"></script>
+    <script src="assets/libs/scrollcue/scrollCue.min.js"></script>
+    <script src="assets/js/vendors/scrollcue.js"></script>
