b81db7d5d6297e12292d98ea20cad136702e9837
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-08-17 00:40:20 +0300

    thank you pages✅

--- thank-you-contact.php
+++ thank-you-contact.php
@@ -2,32 +2,35 @@
 <html lang="en">
 
 <head>
-
-    <title>Thank You | SARL</title>
-
+    <title>Thank You | Contact Us</title>
     <!-- Head Content -->
     <?php include 'includes/head-content.php'; ?>
-
 </head>
 
 <body>
-    <!-- Navbar -->
-    <?php include 'includes/navbar.php'; ?>
 
+    <main class="w-100">
+        <!-- Thank You start -->
+        <div class="container d-flex flex-column overflow-hidden">
+            <div class="row align-items-center justify-content-center min-vh-100 text-center">
+                <div class="col-lg-6 col-12 mb-5">
+                    <div class="position-relative mb-4">
+                        <img src="assets/images/logo/sarl-logo.jpg" alt="Thank You" style="height: 70px;" class="img-fluid" />
+                    </div>
 
-    <main>
-        <!--Page Content-->
+                    <h2>Thank you for reaching out!</h2>
+                    <p>Your message has been received. We will get back to you shortly. In the meantime, feel free to explore our services or visit our homepage.</p>
 
+                    <a href="services.php" class="btn btn-primary">Explore our Services</a>
+                    <a href="index.php" class="btn btn-outline-primary">Go to Home</a>
+                </div>
+            </div>
+        </div>
+        <!-- Thank You end -->
     </main>
-
-    <!-- Footer -->
-    <?php include 'includes/footer.php'; ?>
-
 
     <!-- Scripts -->
     <?php include 'includes/scripts.php'; ?>
-
 </body>
-
 
 </html>
