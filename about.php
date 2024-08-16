23382f6783b2dba396b5b57db40aed554154e857
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-08-16 15:00:22 +0300

    added the boilerplate for each page

---
+++ about.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <title>About Us | SARL</title>
+
+    <!-- Head Content -->
+    <?php include 'includes/head-content.php'; ?>
+
+</head>
+
+<body>
+    <!-- Navbar -->
+    <?php include 'includes/navbar.php'; ?>
+
+
+    <main>
+        <!--Page Content-->
+
+    </main>
+
+    <!-- Footer -->
+    <?php include 'includes/footer.php'; ?>
+
+
+    <!-- Scripts -->
+    <?php include 'includes/scripts.php'; ?>
+
+</body>
+
+
+</html>
---
+++ single-service.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <title>Single Service | SARL</title>
+
+    <!-- Head Content -->
+    <?php include 'includes/head-content.php'; ?>
+
+</head>
+
+<body>
+    <!-- Navbar -->
+    <?php include 'includes/navbar.php'; ?>
+
+
+    <main>
+        <!--Page Content-->
+
+    </main>
+
+    <!-- Footer -->
+    <?php include 'includes/footer.php'; ?>
+
+
+    <!-- Scripts -->
+    <?php include 'includes/scripts.php'; ?>
+
+</body>
+
+
+</html>
