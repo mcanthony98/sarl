90ef3b7834ed19c08b522f33ab952182d222936f
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-08-17 01:27:11 +0300

    single service page✅

--- single-service.php
+++ single-service.php
@@ -3,7 +3,7 @@
 
 <head>
 
-    <title>Single Service | SARL</title>
+    <title>Aluminium & uPVC Fabrications | SARL</title>
 
     <!-- Head Content -->
     <?php include 'includes/head-content.php'; ?>
@@ -17,6 +17,114 @@
 
     <main>
         <!--Page Content-->
+        <div class="pattern-square"></div>
+        <!--Overview start-->
+        <section class="py-5 py-lg-6">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-10 offset-xl-1 col-md-12">
+                        <div class="mb-4">
+                            <a href="services.php" class="text-reset icon-link icon-link-hover">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                                    <path
+                                        fill-rule="evenodd"
+                                        d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
+                                </svg>
+                                Back to Services
+                            </a>
+                        </div>
+
+                        <div class="d-md-flex align-items-center justify-content-between mb-6">
+                            <div class="d-flex align-items-center mb-4 mb-md-0">
+                                <div class="icon-shape icon-xxl border rounded p-4 bg-white">
+                                    <img src="assets/images/logo/sarl-about-logo.jpg" style="height: 40px;" alt="SARL Logo" />
+                                </div>
+                                <div class="ms-3">
+                                    <h1 class="mb-0 h2">Aluminium & uPVC Fabrications</h1>
+                                    <span class="small">Crafting Precision and Excellence in Every Frame</span>
+                                </div>
+
+                            </div>
+                            <div class="d-grid">
+                                <a href="contact.php" class="btn btn-dark">Get this Service</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="offset-xl-1 col-xl-10 col-md-12 col-12">
+                        <!-- Image Slider Start -->
+                        <div id="projectCarousel" class="carousel slide mb-lg-5 mb-md-3 mb-3" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="assets/images/portfolio/portfolio-img-4.jpg" class="d-block w-100 rounded-3" alt="Project Image" style="height: 400px; object-fit: cover;">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="assets/images/portfolio/portfolio-img-2.jpg" class="d-block w-100 rounded-3" alt="Project Image" style="height: 400px; object-fit: cover;">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="assets/images/portfolio/portfolio-img-4.jpg" class="d-block w-100 rounded-3" alt="Project Image" style="height: 400px; object-fit: cover;">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="assets/images/portfolio/portfolio-img-2.jpg" class="d-block w-100 rounded-3" alt="Project Image" style="height: 400px; object-fit: cover;">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="assets/images/portfolio/portfolio-img-4.jpg" class="d-block w-100 rounded-3" alt="Project Image" style="height: 400px; object-fit: cover;">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="assets/images/portfolio/portfolio-img-2.jpg" class="d-block w-100 rounded-3" alt="Project Image" style="height: 400px; object-fit: cover;">
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#projectCarousel" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#projectCarousel" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                        <!-- Image Slider End -->
+                        <div class="mb-5 mb-md-4">
+                            <h2>Overview</h2>
+                            <p class="lead">At SARL, we specialize in Aluminium & uPVC Fabrications that set the standard for durability, aesthetic appeal, and precision. Our services are designed to meet the highest expectations of quality and functionality, transforming spaces with custom solutions that stand the test of time.</p>
+                        </div>
+                        <div class="mb-5 mb-md-4">
+                            <h3>Why Choose Our Fabrications?</h3>
+                            <p>
+                                Our Aluminium & uPVC Fabrications are not just about providing a product; they are about delivering unmatched craftsmanship. Whether it's sleek, modern windows or sturdy doors, our fabrications are designed to enhance the beauty and security of your property. Each piece is custom-built to meet your specific needs, ensuring a perfect fit and finish every time.
+                            </p>
+                            <p>With years of expertise and a commitment to excellence, we bring your vision to life with precision-engineered solutions that elevate any space.</p>
+                        </div>
+                        <div class="mb-5 mb-md-4">
+                            <h3>Our Process</h3>
+                            <p>
+                                From consultation to installation, our process is seamless and customer-focused. We begin with an in-depth understanding of your requirements, followed by meticulous planning and design. Our skilled craftsmen then fabricate each component using state-of-the-art equipment, ensuring that every detail is perfect. Finally, our professional installation team ensures that your new Aluminium & uPVC products are fitted to the highest standards.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--Overview end-->
+
+        <!--Call to action start-->
+        <section>
+            <div class="container py-5">
+                <div class="row">
+                    <div class="col-xl-10 offset-xl-1 col-md-12">
+                        <div class="bg-pattern bg-primary rounded-3 py-5">
+                            <div class="text-center position-relative z-1 py-4">
+                                <h2 class="text-white-stable mb-4">Transform your Space with SARL's Expertise</h2>
+                                <a href="contact.php" class="btn btn-dark">Get This Service Today</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--Call to action end-->
+
 
     </main>
 
