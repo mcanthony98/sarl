78a76cad17d762ff908b0572600d109ee282f315
mcanthony98 <36364125+mcanthony98@users.noreply.github.com>
2024-11-26 23:50:55 +0300

    Update head-content.php

--- includes/head-content.php
+++ includes/head-content.php
@@ -7,6 +7,19 @@
     <link rel="stylesheet" href="assets/libs/swiper/swiper-bundle.min.css" />
     <link rel="stylesheet" href="assets/libs/glightbox/dist/css/glightbox.min.css" />
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/masonry/4.2.2/masonry.pkgd.min.js"></script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            const grid = document.querySelector('#masonry-grid');
+            new Masonry(grid, {
+                itemSelector: '.col-sm-6',
+                columnWidth: '.col-sm-6',
+                percentPosition: true,
+            });
+            });
+
+    </script>
+
     <!-- Favicon icon-->
     <link rel="apple-touch-icon" sizes="180x180" href="assets/images/logo/favicon.jpg" />
     <link rel="icon" type="image/png" sizes="32x32" href="assets/images/logo/favicon.jpg" />
@@ -28,18 +41,7 @@
     <!-- Theme CSS -->
     <link rel="stylesheet" href="assets/css/theme.min.css">
     
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/masonry/4.2.2/masonry.pkgd.min.js"></script>
-    <script>
-        document.addEventListener("DOMContentLoaded", function () {
-            const grid = document.querySelector('#masonry-grid');
-            new Masonry(grid, {
-                itemSelector: '.col-sm-6',
-                columnWidth: '.col-sm-6',
-                percentPosition: true,
-            });
-            });
-
-    </script>
+    
 
     <style>
         /* Project Label */
