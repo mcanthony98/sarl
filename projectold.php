a245332b902aa8257b7ba16efe239e6253b305b9
mcanthony98 <36364125+mcanthony98@users.noreply.github.com>
2025-01-23 18:12:49 +0300

    v

--- projectold.php
+++ projectold.php
@@ -28,55 +28,39 @@ $imgres = $conn->query("SELECT * FROM event_gallery WHERE event_id='$id' LIMIT 1
     <main>
         <!--Page Content-->
 
-         <!--Pageheader start-->
-      <section class="py-10"
-         style="background-image: url(uploads/<?php echo $erow['cover_image'];?>); background-position: center; background-repeat: no-repeat; background-size: cover;filter: brightness(100%);">
-         <div class="container py-lg-10">
-            <div class="row" >
-               <div class="col-lg-6 offset-lg-3 col-md-12 py-2" style="background-color: rgba(0, 0, 0, 0.3);">
-                  <div class="text-center">
-                     <small class="text-white-stable text-uppercase fw-bold ls-lg"><?php echo $erow['category'];?></small>
-                     <h1 class="my-4 text-white-stable display-5"><?php echo $erow['title'];?></h1>
-                     <p class="mb-0 mx-xl-7 text-white-stable lead"><?php echo $erow['short_desc'];?></p>
-                  </div>
-               </div>
+        <!--Pageheader start-->
+        <section class="py-5 py-lg-8 text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 offset-lg-2 col-md-12 col-12">
+                        <h1 class="mb-3"><?php echo $erow['title'];?></h1>
+                        <p class="mb-0 lead"><?php echo $erow['short_desc'];?></p>
+                    </div>
+                </div>
             </div>
-         </div>
-      </section>
-      <!--Pageheader end-->
-      <!--Stats start-->
-      <section class="py-5 mb-5">
-         <div class="container">
-            <div class="row col-10 mx-auto">
-               <div class="col-lg-4">
-                  <div class="text-center px-lg-5 mb-5 mb-lg-0">
-                     <small class="text-uppercase text-body-secondary ls-md fw-semibold">Project</small>
-                     <h4 class="mb-0 mt-2"><?php echo $erow['short_desc'];?></h4>
-                  </div>
-               </div>
-               <div class="col-lg-4">
-                  <div class="text-center px-lg-5 mb-5 mb-lg-0">
-                     <small class="text-uppercase text-body-secondary ls-md fw-semibold">Project Year</small>
-                     <h4 class="mb-0 mt-2"><?php echo $erow['year'];?></h4>
-                  </div>
-               </div>
-               <div class="col-lg-4">
-                  <div class="text-center px-lg-5 mb-5 mb-lg-0">
-                     <small class="text-uppercase text-body-secondary ls-md fw-semibold">Service Offered</small>
-                     <h4 class="mb-0 mt-2"><?php echo $erow['category'];?></h4>
-                  </div>
-               </div>
+        </section>
+        <!--Pageheader end-->
+
+        <!--Project Overview start-->
+        <section class="py-4">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 offset-lg-2 col-md-12 col-12">
+                     
+                        <?php echo $erow['description'];?>
+                      
+                    </div>
+                </div>
             </div>
-         </div>
-      </section>
-      <!--Stats end-->
+        </section>
+        <!--Project Overview end-->
 
         <!--Portfolio Gallery start-->
-        <section class="py-7 mb-9">
+        <section>
             <div class="container">
-                <div class="row gx-2 gy-6 mx-auto" id="masonry-grid">
+                <div class="row gx-2 gy-4 mx-auto" id="masonry-grid">
 
-                    <div class="col-12 mx-auto d-none">
+                    <div class="col-12 mx-auto">
                         <img src="uploads/<?php echo $erow['cover_image'];?>" class="img-fluid rounded-2 gallery-img w-100"  alt="<?php echo $erow['title'];?>" data-bs-target="#imageModal" data-bs-toggle="modal" data-bs-index="0">
                     </div>
 
@@ -91,45 +75,10 @@ $imgres = $conn->query("SELECT * FROM event_gallery WHERE event_id='$id' LIMIT 1
                         <div class="mx-auto <?php echo $class; ?>">
                             <img src="uploads/<?php echo $imgrow['image'];?>" class="img-fluid rounded-1 gallery-img w-100"  style="max-height:600px;" alt="<?php echo $erow['title'];?> - <?php echo $counter;?>" data-bs-target="#imageModal" data-bs-toggle="modal" data-bs-index="<?php echo $counter;?>">
                         </div>
-                    <?php
-                    if($counter == 2 && $erow['description'] != "<p> </p>"){
-                    ?>
-                        <div class="row d-flex align-items-center mt-md-9 mt-6">
-                            <div class="col-lg-6 col-md-6 col-12">
-                                <figure class="mb-4 mb-md-0">
-                                    <img src="uploads/<?php echo $erow['cover_image'];?>" alt="value"
-                                        class="img-fluid rounded-3" style="min-height: 400px;"/>
-                                </figure>
-                            </div>
-                            <div class="col-lg-5 offset-lg-1 col-md-6 col-12">
-                                <span class="text-primary fw-semibold">Project Overview</span>
-                                <div class="mb-4">
-                                    <p class="mb-0 pt-3">
-                                    <?php echo $erow['description'];?>
-                                    </p>
-                                </div>
-                                <a href="contact.php" class="icon-link icon-link-hover">
-                                    Speak to an Expert Today
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor"
-                                        class="bi bi-arrow-right" viewBox="0 0 16 16">
-                                        <path fill-rule="evenodd"
-                                        d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
-                                    </svg>
-                                </a>
-                            </div>
-                            <div class="mt-6 mt-lg-9 text-center">
-                                <h2>Project Gallery</h2>
-                            </div>
-                        </div>
-                    <?php    
-                }
-                    } 
-                    ?>
+                    <?php } ?>
 
-                
                     
                 </div>
-                
             </div>
         </section>
         <!--Portfolio Gallery end-->
