dfcfc69885236cf6968375b0b8416916d96690b9
mcanthony98 <36364125+mcanthony98@users.noreply.github.com>
2025-01-31 12:16:41 +0300

    Update connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -6,4 +6,9 @@ define('DB_PASSWORD', '!5v2DZbvn+WG');
 define('DB_DATABASE', 'u640333703_sarl');
 
 $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}else{
+    echo "Success";
+}
 ?>
