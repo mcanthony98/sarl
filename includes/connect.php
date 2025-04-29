cd53ba3f9b5a475f61f81a8b20f980dceb099028
mcanthony98 <36364125+mcanthony98@users.noreply.github.com>
2025-04-29 08:32:05 +0300

    Update connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_SERVER', 'srv497.hstgr.io');
+define('DB_SERVER', 'srv491.hstgr.io');
 define('DB_USERNAME', 'u640333703_sarl');
 define('DB_PASSWORD', '!5v2DZbvn+WG');
 define('DB_DATABASE', 'u640333703_sarl');
