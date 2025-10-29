a36bb3d227514664ae22b4ae7dae92612ee6b84b
mcanthony98 <36364125+mcanthony98@users.noreply.github.com>
2025-10-29 23:43:29 +0300

    Update connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -8,12 +8,6 @@ define('DB_DATABASE', 'sarlcoke_sarlgtweb');
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-try {
-    $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
-    $conn->set_charset('utf8mb4'); // Always set character encoding
-    echo "Database connection successful!";
-} catch (mysqli_sql_exception $e) {
-    error_log($e->getMessage()); // Log error instead of showing to user
-    exit('Database connection failed.');
-}
+$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+$conn->set_charset('utf8mb4'); // Always set character encoding
 ?>
